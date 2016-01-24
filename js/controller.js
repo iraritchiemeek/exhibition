6399a52003599d09e23dc8493a3a7174221e8c8b
iraritchiemeek <iraritchiemeek@gmail.com>
2016-01-24 13:38:38 +1300

    add text to landingpage

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-	console.log('lets go')
+	
+	var landingPage = new LandingPage
+
+	landingPage.textWrapper()
+	landingPage.textLine('title', 'the_exhibition.')
+	landingPage.textLine('quote', 'i have considered all acts of oppression')
+	landingPage.textLine('subtitle', 'spring/summer1996')
+	landingPage.verticalText('EXHIBITION')
+	landingPage.verticalText('EXHIBITION')
 })
---
+++ js/landing_page.js
@@ -0,0 +1,15 @@
+function LandingPage () {
+	
+}
+
+LandingPage.prototype.textWrapper = function() {
+	$('#container').append('<div id="landing_text_wrapper"></div>')
+};
+
+LandingPage.prototype.textLine = function(id, text) {
+	$('#landing_text_wrapper').append('<div class="text_line" id="' + id + '">' + text + '</div>')
+};
+
+LandingPage.prototype.verticalText = function(text) {
+	$('#container').append('<div class="vertical_text">' + text + '</div>')
+};
