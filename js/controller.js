bc0f34354abd59d0a765f9038bfd9263780900d7
iraritchiemeek <iraritchiemeek@gmail.com>
2016-01-25 19:02:49 +1300

    add homepage image plus fadeout

--- js/controller.js
+++ js/controller.js
@@ -2,12 +2,16 @@ $(document).ready(function () {
 	
 	var landingPage = new LandingPage
 
+	landingPage.appendDiv('fullscreen_image')
+	landingPage.fullScreenImage('imgs/running.png')
+	landingPage.fadeOutLandingImage()
+
 	landingPage.appendDiv('landing_text_wrapper')
 	landingPage.appendDiv('home_menu')
 	landingPage.textLine('title', 'the_exhibition')
-	landingPage.textLine('quote', 'i have considered all acts of oppression')
+	landingPage.textLine('menu', 'collections shop contact')
 	landingPage.textLine('subtitle', 'spring/summer1996')
-	landingPage.verticalText('EXHIBITION')
-	landingPage.verticalText('EXHIBITION')
+	// landingPage.verticalText('EXHIBITION')
+	// landingPage.verticalText('EXHIBITION')
 	landingPage.appendImage('imgs/hugh_mundell.jpg')
 })
--- js/landing_page.js
+++ js/landing_page.js
@@ -2,6 +2,17 @@ function LandingPage () {
 	
 }
 
+LandingPage.prototype.fullScreenImage = function(url) {
+	$('#fullscreen_image').css({'backgroundImage':'Url(' + url + ')'})
+};
+
+LandingPage.prototype.fadeOutLandingImage = function() {
+	function fade () {
+		$('#fullscreen_image').fadeOut('slow')
+	}
+	setTimeout(fade, 4000);
+};
+
 LandingPage.prototype.appendDiv = function(id) {
 	$('#container').append('<div id="' + id + '"></div>')
 };
